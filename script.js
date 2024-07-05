f6bac1b9d4c147362823214e8c9d7afa3e0ea8b1
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-07-06 01:21:51 +0200

    Aggiunge implementazione parziale di interazione e animazione pulsanti

--- script.js
+++ script.js
@@ -71,5 +71,72 @@ function changeDisplayText(string){
 
 }
 
+//funzione che cancella
+function eraseDisplayText(all){
+    if(all === true){
+        displayPar.textContent = "";
+    }else{
+        displayPar.textContent = displayPar.textContent.slice(0, -1);
+    }
+}
+
 
 //eventListener:
+padDiv.addEventListener("click", (e)=>{
+    let target = e.target;
+    switch(target.id){
+        case "one":
+            changeDisplayText("1");
+            break;
+        case "two":
+            changeDisplayText("2");
+            break;
+        case "three":
+            changeDisplayText("3");
+            break;
+        case "four":
+            changeDisplayText("4");
+            break;
+        case "five":
+            changeDisplayText("5");
+            break;
+        case "six": 
+            changeDisplayText("6");
+            break;
+        case "seven":
+            changeDisplayText("7");
+            break;
+        case "eight":
+            changeDisplayText("8");
+            break;
+        case "nine":
+            changeDisplayText("9");
+            break;
+        case "zero":
+            changeDisplayText("0");      
+            break;
+        case "clear":
+            eraseDisplayText(true);  
+            break;
+        case "delete":
+            eraseDisplayText(false);
+            break; 
+        case                                    
+    }
+
+
+});
+
+pad.addEventListener("mousedown", (e)=>{
+    let target = e.target;
+    if(target.classList.contains("button")){
+        target.style.backgroundImage = "url(./img/black-mental-background.jpg)";
+    }
+});
+
+pad.addEventListener("mouseup", (e)=>{
+    let target = e.target;
+    if(target.classList.contains("button")){
+        target.style.backgroundImage = "url(./img/metallic1.jpg)";
+    }
+});
